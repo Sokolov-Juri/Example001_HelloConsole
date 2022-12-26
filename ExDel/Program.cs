38f7f577b689fbe4233240462e3495ef96a54153
Sokolov Juri <sokolov-juri@yandex.ru>
2022-12-26 22:24:11 +0300

    exdel

--- ExDel/Program.cs
+++ ExDel/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-double numberA = 12;
+double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
