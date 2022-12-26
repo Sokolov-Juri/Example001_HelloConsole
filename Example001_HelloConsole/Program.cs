5eab95ca2f2d715eecbcd8da9d92322778135d61
Sokolov Juri <sokolov-juri@yandex.ru>
2022-12-26 21:41:30 +0300

    all

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, 2 World!");
+Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
+
+
